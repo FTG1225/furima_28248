d06325fdd90e4fd4cfe5528fe1f1fc7fb49a963f
FTG1225 <ftg92759762@gmail.com>
2020-08-13 19:43:56 +0900

    指摘箇所の修正（re_push）

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,11 +3,11 @@ FactoryBot.define do
     association :user
     name { 'Yamada' }
     description { 'This is an Apple' }
-    category_id       { Faker::Number.between(from: 2, to: 10) }
-    status_id         { Faker::Number.between(from: 2, to: 7) }
-    shipping_tax_id   { Faker::Number.between(from: 2, to: 3) }
-    prefecture_id     { Faker::Number.between(from: 2, to: 48) }
-    shipping_date_id  { Faker::Number.between(from: 2, to: 4) }
+    category_id       { 2 }
+    status_id         { 2 }
+    shipping_tax_id   { 2 }
+    prefecture_id     { 2 }
+    shipping_date_id  { 2 }
     price             { 1000 }
     images { Rack::Test::UploadedFile.new(File.join(Rails.root, 'public/images/test_image.png')) }
   end
